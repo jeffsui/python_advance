eb324c3953488cafabdee780d5f6bfea1cb7efaf
jeffsui <pinghailinfeng79@gmail.com>
2025-06-13 10:10:25 +0800

    feat: update TypeDict 新用法, 支持NotRequired和Required标注

--- type_annotations/typed_dict_demo.py
+++ type_annotations/typed_dict_demo.py
@@ -4,7 +4,7 @@ TypedDict is a way to specify the expected structure of a dictionary.
 It allows you to define a dictionary with specific keys and their corresponding value types.
 
 """
-from typing import List, Optional, TypedDict
+from typing import List, NotRequired, Optional, Required, TypedDict
 
 
 class Person(TypedDict):
@@ -68,3 +68,40 @@ if __name__ == "__main__":
     # Example with all fields
     employee_info_full: Employee = {"name": "Diana", "age": 40, "position": "Project Manager", "department": "IT"}
     print_employee_info(employee_info_full)
+
+# 3.11 support Required NotRequired
+class Print2D(TypedDict):
+    x:int
+    y:int
+    label: NotRequired[str]
+
+def print2D(print2d: Print2D):
+    print(f'x:{print2d["x"]}')
+    print(f'y:{print2d["y"]}')
+# 3.11 中 可以设置total属性,false代表所有属性都不是必须的
+class Print3D(TypedDict,total=False):
+    x:Required[int]
+    y:Required[int]
+    z:Required[int]
+    label: NotRequired[str]
+
+# 3.13 中新增了 TypeAlias(类型别名的方式,我个人倾向用类语法实现)
+# Print3D = TypedDict(
+#     "Print3D",
+#     {
+#         'x':int,
+#         'y':int,
+#         'z':int,
+#         'label': str
+#     }
+# )
+
+def print3D(print3d: Print3D):
+    print(f'x:{print3d["x"]}')
+    print(f'y:{print3d["y"]}')
+    print(f'z:{print3d["z"]}')
+    print(f'label:{print3d["label"]}')
+
+if __name__ == '__main__':
+    print2D({'x':1,'y':2})
+    print3D({'x':3,'y':4,'z':5,'label':"3D"})
